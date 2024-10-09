f6d2e1377297994dab9f6a644e253e253293d175
Danielroxs <danroxsll@gmail.com>
2024-10-08 18:02:47 -0600

    animation cards

--- src/sections/AboutUs.jsx
+++ src/sections/AboutUs.jsx
@@ -1,21 +1,73 @@
 import React, { useEffect } from "react";
 import { FaInstagram, FaFacebook, FaYoutube } from "react-icons/fa";
-import neri from "../assets/images/neri.webp";
 import ProfileCard from "../components/ProfileCard";
-import { useInView } from "react-intersection-observer";
 import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import FadeInText from "../components/Motion";
 
 const AboutUs = () => {
-  const ServiceCard = ({ title, description, icon }) => {
+  const ServiceCard = ({ title, description, icon, index }) => {
+    const controls = useAnimation();
+    const [ref, inView] = useInView({
+      triggerOnce: true,
+      threshold: 0.5,
+    });
+
+    useEffect(() => {
+      if (inView) {
+        controls.start("visible");
+      }
+    }, [controls, inView]);
+
+    const variants = {
+      hidden: { y: 50, opacity: 0 },
+      visible: {
+        y: 0,
+        opacity: 1,
+        transition: {
+          type: "spring",
+          bounce: 0.4,
+          duration: 0.8,
+          delay: index * 0.2, // adding staggered effect using index
+        },
+      },
+    };
+
     return (
-      <div className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+      <motion.div
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={variants}
+        className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+      >
         <div className="text-4xl mb-4">{icon}</div>
         <h3 className="text-xl font-semibold text-gray-900 mb-2">{title}</h3>
         <p className="text-gray-700">{description}</p>
-      </div>
+      </motion.div>
     );
   };
+
+  const services = [
+    {
+      title: "Planes de nutrición personalizados",
+      description:
+        "Planes de comidas personalizados diseñados para satisfacer sus objetivos de salud y preferencias dietéticas específicos.",
+      icon: "🥗",
+    },
+    {
+      title: "Formación individual",
+      description:
+        "Sesiones de entrenamiento individualizadas centradas en su nivel de forma física y sus objetivos",
+      icon: "💪",
+    },
+    {
+      title: "Talleres de bienestar",
+      description:
+        "Seminarios educativos sobre diversos temas de salud para que adquiera conocimientos.",
+      icon: "📚",
+    },
+  ];
 
   return (
     <div className="min-h-screen lg:text-left text-center py-12 px-4 sm:px-6 lg:px-8">
@@ -46,6 +98,7 @@ const AboutUs = () => {
                 href="https://www.facebook.com/share/uG6KzuhtfF5o4jZD/?mibextid=LQQJ4d"
                 target="_blank"
                 className="text-blue-600 hover:text-blue-800 transition-colors duration-300"
+                rel="noopener noreferrer"
               >
                 <FaFacebook size={24} />
               </a>
@@ -53,6 +106,7 @@ const AboutUs = () => {
                 href="https://www.instagram.com/nerii_villeda.fit/"
                 className="text-pink-600 hover:text-pink-800 transition-colors duration-300"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <FaInstagram size={24} />
               </a>
@@ -60,21 +114,14 @@ const AboutUs = () => {
                 href="https://www.youtube.com/@NeriPrideMusicOficial/videos"
                 className="text-red-600 hover:text-red-800 transition-colors duration-300"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <FaYoutube size={24} />
               </a>
             </div>
           </div>
           <div className="lg:w-1/2">
-            <div className="relative w-64 h-64 mx-auto lg:w-80 lg:h-80">
-              {/* <div className="absolute inset-0 bg-gradient-to-br from-green-400 to-blue-500 rounded-full animate-pulse"></div>
-              <img
-                src={neri}
-                alt="Neri Villeda"
-                className="absolute inset-2 w-60 h-60 lg:w-76 lg:h-76 object-cover rounded-full"
-                /> */}
-              <ProfileCard />
-            </div>
+            <ProfileCard />
           </div>
         </div>
         <div className="mt-12">
@@ -82,21 +129,15 @@ const AboutUs = () => {
             Nuestros servicios
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            <ServiceCard
-              title="Planes de nutrición personalizados"
-              description="Planes de comidas personalizados diseñados para satisfacer sus objetivos de salud y preferencias dietéticas específicos."
-              icon={"🥗"}
-            />
-            <ServiceCard
-              title="Formación individual"
-              description="Sesiones de entrenamiento individualizadas centradas en su nivel de forma física y sus objetivos"
-              icon={"💪"}
-            />
-            <ServiceCard
-              title="Talleres de bienestar"
-              description="Seminarios educativos sobre diversos temas de salud para que adquiera conocimientos."
-              icon={"📚"}
-            />
+            {services.map((service, index) => (
+              <ServiceCard
+                key={index}
+                title={service.title}
+                description={service.description}
+                icon={service.icon}
+                index={index} // passing index for staggered animation
+              />
+            ))}
           </div>
         </div>
       </div>
